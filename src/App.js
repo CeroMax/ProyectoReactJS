f17b6c456536e10fd836fdf39124713ed89809d8
Max <calderondavid1973@gmail.com>
2022-09-03 19:57:15 -0300

    fotos

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 export function App() {
   return (
-    <>
+    <BrowserRouter>
       <header className="App-header">
           <NavBar/>
+          www.miTienda.com/
       </header>
+      <Routes>
+        <Route path='/' element={<ItemListContainer/>}/>
+        <Route/>
+        <Route/>
+      </Routes>
       <body>
         <h1>"LALALALALAL"</h1>
       </body>
       <footer>
 
       </footer>
-    </>
+    </BrowserRouter>
   );
 }
 
