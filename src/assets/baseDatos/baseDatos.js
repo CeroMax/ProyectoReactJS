e69a5c9bf1f55b1402421f3c47fdad10080f2ed8
Max <calderondavid1973@gmail.com>
2022-09-03 21:27:18 -0300

    ver

--- src/assets/baseDatos/baseDatos.js
+++ src/assets/baseDatos/baseDatos.js
@@ -2,19 +2,19 @@
 import sierraElectrica from'../fotos/herramientaelectrica1-min.png';
 import lijadoraElectrica from '../fotos/herramientaelectrica3-min.png';
 import amoladoraElectrica from '../fotos/herramientaelectrica6-min.png';
-import taladroElectrico from '../fotos/herramientaelectrica7-min.png';
-import llavesFijas from '../fotos/herramientaMano3-min.png';
-import juegoDestornilladores from '../fotos/herramientaMano4-min.png';
-import kitHerramientas from '../fotos/herramientaMano5-min.png';
-import kitPinzas from '../fotos/herramientaMano5-min.png';
+//import taladroElectrico from '../fotos/herramientaelectrica7-min.png';
+//import llavesFijas from '../fotos/herramientaMano3-min.png';
+//import juegoDestornilladores from '../fotos/herramientaMano4-min.png';
+//import kitHerramientas from '../fotos/herramientaMano5-min.png';
+//import kitPinzas from '../fotos/herramientaMano5-min.png';
 
 export const productosFerreteria = [
-    {id:1,nombre:"herramienta mano",precio:"$ 100",foto:llavesFijas,tipoHerramienta:"herramienta de mano"},
-    {id:2,nombre:"herramienta mano",precio:"$ 100",foto:juegoDestornilladores,tipoHerramienta:"herramienta de mano"},
-    {id:3,nombre:"herramienta mano",precio:"$ 100",foto:kitHerramientas,tipoHerramienta:"herramienta de mano"},
-    {id:4,nombre:"herramienta mano",precio:"$ 100",foto:kitPinzas,tipoHerramienta:"herramienta de mano"},
+    //{id:1,nombre:"herramienta mano",precio:"$ 100",foto:llavesFijas,tipoHerramienta:"herramienta de mano"},
+    //{id:2,nombre:"herramienta mano",precio:"$ 100",foto:juegoDestornilladores,tipoHerramienta:"herramienta de mano"},
+    //{id:3,nombre:"herramienta mano",precio:"$ 100",foto:kitHerramientas,tipoHerramienta:"herramienta de mano"},
+    //{id:4,nombre:"herramienta mano",precio:"$ 100",foto:kitPinzas,tipoHerramienta:"herramienta de mano"},
     {id:5,nombre:"herramienta electrica",precio:"$ 100",foto:sierraElectrica,tipoHerramienta:"herramienta electrica"},
     {id:6,nombre:"herramienta electrica",precio:"$ 100",foto:lijadoraElectrica,tipoHerramienta:"herramienta electrica"},
     {id:7,nombre:"herramienta electrica",precio:"$ 100",foto:amoladoraElectrica,tipoHerramienta:"herramienta electrica"},
-    {id:8,nombre:"herramienta electrica",precio:"$ 100",foto:taladroElectrico,tipoHerramienta:"herramienta electrica"},
+    //{id:8,nombre:"herramienta electrica",precio:"$ 100",foto:taladroElectrico,tipoHerramienta:"herramienta electrica"},
 ]
